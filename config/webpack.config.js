7b3e70562a2f6314a40429101b2bbc15c4e3c88d
lcb <liuchengbin@laoyuegou.com>
2019-05-29 15:20:12 +0800

    Initial commit

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,63 +1,70 @@
 const path = require("path");
-const base = path.join(__dirname, "..");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWepackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+/**
+ * 开发环境(development)和生产环境(production)的构建目标差异很大。
+ * 在开发环境中，我们需要具有强大的、具有实时重新加载(live reloading)
+ * 或热模块替换(hot module replacement)能力的 source map 和 localhost server。
+ * 而在生产环境中，我们的目标则转向于关注更小的 bundle，更轻量的 source map，以及更优化的资源，以改善加载时间。
+ * 由于要遵循逻辑分离，我们通常建议为每个环境编写彼此独立的 webpack 配置。
+ */
 
 module.exports = {
-  mode: "production",
-  entry: path.resolve(base, "src", "index.js"),
-  devtool: 'cheap-module-source-map',
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(base, "dist")
-  },
-  resolve: {
-    extensions: [".js", ".jsx"]
-  },
-  devServer: {
-    /**
-    1. contentBase 指定项目根目录
-    2. historyApiFallback 主要应用在单页应用的开发场景，它依赖于 HTML5 history API，如果设置为 true，所有的跳转将指向 index.html
-    3. inline 主要是解决了自动浏览器自动刷新问题
-    4. lazy 默认为 false，若开启后，则 webpack-dev-server 不再监测文件变化自动编译，只有等到我们手动刷新浏览器的时候才会编译文件
-    5. proxy 可以有效的解决开发阶段的跨域问题，毕竟不是所有的前端项目，都有独立的 nodejs 作为中间件去请求服务，更多的还是把前端项目编译后与后端服务部署在一起。而开发阶段又相对独立，这时候就可以利用 proxy 将前端请求转发到后端测试服务器，不影响开发
-    **/
-    contentBase: path.resolve(base, 'dist'),
-    host: '127.0.0.1',
-    port: 3000,
-    historyApiFallback: true,
-    inline: true,
-    proxy: {
-      "/api": "http://localhost:8000"
+    entry: {
+        app: './src/index.js'
     },
-    stats: {
-      colors: true,
+    output: {
+        // filename: 'bundle.js',
+        // 根据入口起点名称动态生成bundle名称
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, '../dist'),
+        publicPath: '/'
     },
-  },
-  module: {
-	rules: [
-	  {
-	    test: /\.jsx?$/,
-	    exclude: /node_modules/,
-	    use: {
-	      loader: 'babel-loader',
-	      options: { // babel 转义的配置选项
-	        babelrc: false,
-	        presets: [
-	          require.resolve('@babel/preset-react'),
-	          [require.resolve('@babel/preset-env'), { modules: false }],
-	        ],
-	        cacheDirectory: true,
-	      },
-	    },
-	  },
-	],
-	},
-  plugins: [
-	new HtmlWebPackPlugin({
-	  template: "src/index.html",
-	  filename: "index.html"
-	})
-  ]
-};
-
+    plugins: [
+        new HtmlWebpackPlugin({template:'./public/index.html'}),
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx'], // 自动解析 js jsx 扩展名
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                  options: { // babel 转义的配置选项
+                    babelrc: false,
+                    presets: [
+                      require.resolve('@babel/preset-react'),
+                      [require.resolve('@babel/preset-env'), { modules: false }],
+                    ],
+                    cacheDirectory: true,
+                  },
+                },
+            },
+            {
+                test: /\.(css|less)$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'less-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    }
+}
